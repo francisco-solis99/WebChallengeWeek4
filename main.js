bd9f22f65bc485ee59316dd2f7fbf83e1b1047a9
Francisco Solis <franciscosoliscss@gmail.com>
2020-09-17 10:41:50 -0500

    [Add] slides info into js

---
+++ main.js
@@ -0,0 +1,27 @@
+// SLIDES INFO
+const SLIDES = [
+    {
+
+        texto: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
+        src: './images/image-tanya.jpg',
+        name: 'Tanya Sinclair',
+        job: 'UX Engineer'
+
+
+    }, 
+
+    {
+
+        texto:'"If you want to lay the best foundation possible I’d recommend takng this course. The depth te instructors go into is incredible. I now feel so confident about starting up as a professional developer "',
+        src: './images/image-john.jpg',
+        name: 'Jonh Tarkpor',
+        job: 'Junior Front-end Developer'
+
+    }
+]
+
+
+// console.log(SLIDES[0].texto)
+
+
+
